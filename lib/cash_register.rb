e026d35781ec472ed0f48e0f76a73bba004163df
nicholasstano <nicholasstano@gmail.com>
2019-06-26 15:33:16 -0400

    changes

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+require 'pry'
+require 'byebug'
+
+class CashRegister
+
+	attr_accessor :total, :discount, :title, :price
+	
+
+	def initialize(total = 0)
+		@total = total
+		@discount = 20
+	end
+
+	# def add_item(title, price)
+	# 	@title = title
+	# 	@price = price
+
+	# end
+
+	# def add_item(title, price, quantity = "".to_i)
+	# 	@total = total + price.to_i
+	# 	@total
+	# end
+
+	
+
+
+end
 
