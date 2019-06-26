6a35b6da21f07f762dc1077e73777d9c7f440073
nicholasstano <nicholasstano@gmail.com>
2019-06-26 17:03:04 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,27 +3,40 @@ require 'byebug'
 
 class CashRegister
 
-	attr_accessor :total, :discount, :title, :price
-	
+	attr_accessor :total, :discount, :items, :last_price
 
-	def initialize(total = 0)
-		@total = total
-		@discount = 20
+	def initialize(discount = 0)
+		@total = 0
+		@discount = discount
+		@items = []
+		@last_price = last_price
 	end
 
-	# def add_item(title, price)
-	# 	@title = title
-	# 	@price = price
-
-	# end
-
-	# def add_item(title, price, quantity = "".to_i)
-	# 	@total = total + price.to_i
-	# 	@total
-	# end
-
+	def add_item(title, price, quantity = 1)
 	
+		@total = total + price * quantity
+		quantity.times do 
+			@items << title
+		end
+		@last_price = price
+	end
 
+	def apply_discount
+		@total = total - total * discount/100 
+		if discount == 0 
+			"There is no discount to apply."
+		else
+			"After the discount, the total comes to $#{total}."
+		end
+	end
+
+	def items
+		@items
+	end
+
+	def void_last_transaction
+		@total = total - last_price
+	end
 
 end
 
